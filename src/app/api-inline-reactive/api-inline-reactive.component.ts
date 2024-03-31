bd89a5997f76992ec38eefd99ebececb740df8b4
Shubham Bahre <shubham.bahre@viklele.com>
2024-03-31 22:22:36 +0530

    Working table and pagination

--- src/app/api-inline-reactive/api-inline-reactive.component.ts
+++ src/app/api-inline-reactive/api-inline-reactive.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {
   FormArray,
   FormBuilder,
@@ -9,14 +9,26 @@ import {
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCardModule } from '@angular/material/card';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { UserService } from '../services/user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+export interface User {
+  //isSelected: boolean;
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  birthDate: string;
+  isEdit: boolean;
+}
 
 @Component({
   selector: 'app-api-inline-reactive',
@@ -33,13 +45,28 @@ import { HttpClientModule } from '@angular/common/http';
     MatProgressSpinnerModule,
     MatButtonModule,
     HttpClientModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
   ],
   templateUrl: './api-inline-reactive.component.html',
   styleUrl: './api-inline-reactive.component.scss',
 })
 export class ApiInlineReactiveComponent implements OnInit {
   tableForm!: FormGroup;
-  columns = ['firstName', 'lastName', 'gender', 'email', 'birthDate'];
+  displayedColumns = [
+    'isSelected',
+    'firstName',
+    'lastName',
+    'gender',
+    'email',
+    'birthDate',
+    'isEdit',
+  ];
+  dataSource = new MatTableDataSource<User>();
+  @ViewChild('tablePaginator') paginator!: MatPaginator;
 
   constructor(private fb: FormBuilder, private userService: UserService) {
     this.tableForm = new FormGroup({
@@ -48,8 +75,8 @@ export class ApiInlineReactiveComponent implements OnInit {
   }
 
   async ngOnInit() {
-    let users = await this.userService.getUsersAsync();
-    console.log(users);
+    this.dataSource.data = await this.userService.getUsersAsync();
+    this.dataSource.paginator = this.paginator;
   }
 
   addRow(): FormGroup {
@@ -80,5 +107,36 @@ export class ApiInlineReactiveComponent implements OnInit {
   onDeleteClick(index: number) {
     let formArray = this.getFormArray();
     formArray!.removeAt(index);
+  }
+
+  isAllSelected() {
+    return this.dataSource.data.every((item: any) => item.isSelected);
+  }
+
+  isAnySelected() {
+    return this.dataSource.data.some((item: any) => item.isSelected);
+  }
+
+  selectAll(event: any) {
+    this.dataSource.data = this.dataSource.data.map((item: any) => ({
+      ...item,
+      isSelected: event.checked,
+    }));
+  }
+
+  async removeRow(id: number) {
+    /**Observable way */
+
+    // this.userService.deleteUser(id).subscribe(() => {
+    //   this.dataSource.data = this.dataSource.data.filter(
+    //     (u: User) => u.id !== id
+    //   );
+    // });
+    /**Promise way */
+    await this.userService.deleteUserAsync(id);
+    this.dataSource.data = this.dataSource.data.filter(
+      (u: User) => u.id !== id
+    );
+    this.dataSource.data = [...this.dataSource.data];
   }
 }
